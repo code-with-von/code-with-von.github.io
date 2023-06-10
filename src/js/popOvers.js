f3b19881a08b5ccab6a6a8b1e1281c2c573a5045
von Schappler <37213588+vonschappler@users.noreply.github.com>
2023-06-10 11:16:24 -0300

    Add source code

---
+++ src/js/popOvers.js
@@ -0,0 +1,6 @@
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]'
+);
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
